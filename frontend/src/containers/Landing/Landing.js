562eccbf4c589dda2ec01f7068664c08ed6f4b15
Panagiotis Ganelis <panagiotis.ganelis-cic.netherlands@ibm.com>
2019-11-25 17:06:23 +0100

    add middleware and validation

--- frontend/src/containers/Landing/Landing.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import Counter from "../../components/Counter/Counter";
-
-const Landing = () => {
-  const [count, setCount] = useState(0);
-  return <Counter count={count} setCount={setCount} />;
-};
-
-export default Landing;
