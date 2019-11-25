78bb5ae0711df07e5ade99b4a6cbec80f010abc9
Panagiotis Ganelis <panagiotis.ganelis-cic.netherlands@ibm.com>
2019-11-25 13:55:03 +0100

    create counter component

---
+++ frontend/src/components/Counter.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./Counter.css";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="counter">
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+};
+export default Counter;
